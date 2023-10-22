20159cdbc66fc392ad9e251dd55b2553bbd3fa65
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-10-23 02:26:35 +0800

    finished lessons 7-8

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,2 +1,24 @@
-const str = 'str';
-console.log(str);
+//lesson 7-8
+// const str = 'str';
+// console.log(str);
+//
+//
+// let userName: string = 'Ivan';
+// userName = '5';
+let isBirthday = true;
+let age = 40;
+let userName = 'John';
+function logBrtMsg(isBirthday, userName, age) {
+    if (isBirthday) {
+        console.log("Congrats ".concat(userName, ", ").concat(age + 1));
+    }
+}
+logBrtMsg(isBirthday, userName, age);
+// if (isBirthday) {
+//   console.log(`Congrats ${userName}, ${age + 1}`);
+// }
+//
+//ts-node chapter1/index.ts
+//tsc chapter1/index.ts
+//tsc --init
+// tsc -help
