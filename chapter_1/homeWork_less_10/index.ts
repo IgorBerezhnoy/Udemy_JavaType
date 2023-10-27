74df5abe1580ddf652905bcfb3b83e95815b4ed4
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-10-27 22:15:57 +0800

    finished lessons 11-13

--- chapter_1/homeWork_less_10/index.ts
+++ chapter_1/homeWork_less_10/index.ts
@@ -1,20 +1,20 @@
-const currRate :string= "1.05";
-
-const fetchCurr = (response:string) => {
-	return JSON.parse(response);
-};
-
-function transferEurToUsd(available:boolean, amount:number, commission:number) {
-	if (available) {
-		let res = fetchCurr(currRate) * amount * commission;
-		console.log(res);
-		// Или запись в элемент на странице вместо консоли
-	} else {
-		console.log("Сейчас обмен недоступен");
-	}
-}
-
-transferEurToUsd(true, 500, 1.05);
-
-
-// tsc chapter_1/homeWork_less_10/index.ts
+// const currRate :string= "1.05";
+//
+// const fetchCurr = (response:string) => {
+// 	return JSON.parse(response);
+// };
+//
+// function transferEurToUsd(available:boolean, amount:number, commission:number):void {
+// 	if (available) {
+// 		let res = fetchCurr(currRate) * amount * commission;
+// 		console.log(res);
+// 		// Или запись в элемент на странице вместо консоли
+// 	} else {
+// 		console.log("Сейчас обмен недоступен");
+// 	}
+// }
+//
+// transferEurToUsd(true, 500, 1.05);
+//
+//
+// // tsc chapter_1/homeWork_less_10/index.ts
