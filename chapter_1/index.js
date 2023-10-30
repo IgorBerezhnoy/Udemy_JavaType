e76d86b96ef3def2ece84c5ee077d2fefcef01d4
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-10-30 15:50:24 +0800

    finished lessons #14-15

--- chapter_1/index.js
+++ chapter_1/index.js
@@ -2,19 +2,84 @@
 // //tsc chapter_1/index.ts
 // //tsc --init
 // // tsc -help
-
-// //lesson 9
-let userData = "{ isBirthday: true, age: 40, userName: 'John' }";
-let userObj=JSON.parse(userData)
-let logBrtMsg = function (isBirthday, userName, age) {
-    if (isBirthday) {
-        return "Congrats ".concat(userName, ", ").concat(age + 1);
+var userData = {
+    isBirthday: true,
+    age: 40,
+    userName: 'John',
+};
+var createError = function (msg) {
+    throw new Error(msg);
+};
+// let userData = {isBirthday: true, age: 40, userName: 'John'};
+// let userObj: ObjType = JSON.parse(userData);
+var logBrtMsg = function (userData) {
+    if (userData.isBirthday) {
+        return "Congrats ".concat(userData.userName, ", age: ").concat(userData.age + 1);
     }
     else {
-        return 'Error';
+        return createError('Error');
     }
 };
-console.log(logBrtMsg(userObj.isBirthday, userObj.userName, userObj.age));
+console.log(logBrtMsg(userData));
+//
+// //lesson 12
+//
+// const test: null = null;
+// // const test2: string = null;//strictNullChecks
+// // const test4: number = null;////strictNullChecks
+//
+// function getRndData() {
+//   if (Math.random() < 0.5) {
+//     return null;
+//   } else {
+//     return '  Some string';
+//   }
+// }
+//
+// const data = getRndData();
+// const trimmedData = data?.trim();
+// //lesson 11
+// type ObjType = { isBirthday: boolean, userName: string, age: number };
+// const isBirthday = true;
+// const age = 40;
+// const userName = 'John';
+// const createError = (msg: string): never => {
+//   throw new Error(msg);
+// };
+// // let userData = {isBirthday: true, age: 40, userName: 'John'};
+// // let userObj: ObjType = JSON.parse(userData);
+//
+//
+// const logBrtMsg = (isBirthday: boolean, userName: string, age: number): string => {
+//   if (isBirthday) {
+//     return `Congrats ${userName}, ${age + 1}`;
+//   } else if (!isBirthday) {
+//     return createError('Error');
+//     // return 'Too bad';
+//   }
+// };
+//
+// console.log(logBrtMsg(isBirthday, userName, age));
+//
+// //lesson 9
+// let salary:number;
+// salary = 5000;
+//
+//
+// type ObjType = { isBirthday: boolean, userName: string, age: number };
+// let userData = '{ isBirthday: true, age: 40, userName: \'John\' }';
+// let userObj: ObjType = JSON.parse(userData);
+//
+//
+// const logBrtMsg = (isBirthday: boolean, userName: string, age: number): string => {
+//   if (isBirthday) {
+//     return `Congrats ${userName}, ${age + 1}`;
+//   } else {
+//     return 'Error';
+//   }
+// };
+//
+// console.log(logBrtMsg(userObj.isBirthday, userObj.userName, userObj.age));
 // //lesson 7-8
 //
 // // const str = 'str';
--- chapter_1/index.ts
+++ chapter_1/index.ts
@@ -3,22 +3,65 @@
 // //tsc --init
 // // tsc -help
 
-// //lesson 12
-
-const test: null = null;
-// const test2: string = null;//strictNullChecks
-// const test4: number = null;////strictNullChecks
-
-function getRndData() {
-  if (Math.random() < 0.5) {
-    return null;
-  } else {
-    return '  Some string';
+// //lesson #14-15
+type ObjType = {
+  isBirthday: boolean, userName: string, age: number, messages: {
+    error: string
   }
-}
+};
+const userData = {
+  isBirthday: true,
+  age: 40,
+  userName: 'John',
+  messages: {
+    error: 'Error'
+  }
+};
 
-const data = getRndData();
-const trimmedData = data?.trim();
+const createError = (msg: string): never => {
+  throw new Error(msg);
+};
+// let userData = {isBirthday: true, age: 40, userName: 'John'};
+// let userObj: ObjType = JSON.parse(userData);
+
+
+const logBrtMsg = ({isBirthday, userName, age, messages:{error}}: ObjType): string => {
+  if (isBirthday) {
+    return `Congrats ${userName}, age: ${age + 1}`;
+  } else {
+    return createError(error);
+  }
+};
+// const logBrtMsg = (userData: ObjType): string => userData.isBirthday
+//   ? `Congrats ${userData.userName}, age: ${userData.age + 1}`
+//   : createError('Error');
+console.log(logBrtMsg(userData));
+//
+
+const departments: string[] = ['dev', 'design', 'marketing'];
+const department: string = departments[0];
+// departments.push(5)
+const report = departments.filter(el => el !== 'dev').map(el => `${el} - done`);
+
+const nums: number[][] = [[1, 2], [3, 4], [5]];
+const [first] = report;
+console.log(first);
+// //lesson 12
+//
+// const test: null = null;
+// // const test2: string = null;//strictNullChecks
+// // const test4: number = null;////strictNullChecks
+//
+// function getRndData() {
+//   if (Math.random() < 0.5) {
+//     return null;
+//   } else {
+//     return '  Some string';
+//   }
+// }
+//
+// const data = getRndData();
+// const trimmedData = data?.trim();
 
 // //lesson 11
 
