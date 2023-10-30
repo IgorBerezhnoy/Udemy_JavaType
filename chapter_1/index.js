6b52b5b36ae596f4eab737b33f4708f2c7ef8aee
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-10-30 18:01:54 +0800

    finished lessons #19-22

--- chapter_1/index.js
+++ chapter_1/index.js
@@ -2,26 +2,201 @@
 // //tsc chapter_1/index.ts
 // //tsc --init
 // // tsc -help
-var userData = {
-    isBirthday: true,
-    age: 40,
-    userName: 'John',
-};
-var createError = function (msg) {
-    throw new Error(msg);
-};
-// let userData = {isBirthday: true, age: 40, userName: 'John'};
-// let userObj: ObjType = JSON.parse(userData);
-var logBrtMsg = function (userData) {
-    if (userData.isBirthday) {
-        return "Congrats ".concat(userData.userName, ", age: ").concat(userData.age + 1);
+// //lesson #20 Примитивные литеральные типы (Literal types)
+var msg = 'hello';
+msg = 'Hello';
+var port3000 = 3000;
+var port3001 = 3001;
+function startServer(protocol, port) {
+    if (port === port3000 || port === port3001) {
+        console.log("Serer started in ".concat(protocol, ": //server:").concat(port));
     }
     else {
-        return createError('Error');
+        console.log('Error!!!');
+    }
+    return 'Serer started';
+}
+var createAnimation = function (id, animationName, timingFunc, duration, iterCount) {
+    if (timingFunc === void 0) { timingFunc = 'ease'; }
+    var elem = document.querySelector("#".concat(id));
+    if (elem && elem.style) {
+        console.log("".concat(animationName, " ").concat(timingFunc, " ").concat(duration, " ").concat(iterCount));
+        // elem.style.animation = `${animationName} ${timingFunc} ${duration} ${iterCount}`;
+        // console.log(elem.style.animation);
     }
 };
-console.log(logBrtMsg(userData));
+// //lesson #19 Сужение типов (Narrowing)
 //
+//
+// let message: string | number = '';
+// let messages: string[] | number[] = ['m', 'ss'];
+// //
+// // function printMess(msg: string | number|boolean) {
+// //   if (typeof msg !== 'number'||typeof msg==="number") {
+// //     console.log(msg.toString());
+// //   } else {
+// //     console.log(msg);
+// //   }
+// //   console.log(msg)
+// // }
+// function printMess(msg: string[] | number | boolean) {
+//   if (Array.isArray(msg)) {
+//     msg.forEach(el => console.log(el));
+//   } else if (typeof msg === 'number') {
+//     console.log(msg.toFixed());
+//   } else {
+//     console.log(msg);
+//   }
+// }
+//
+// const printReadings = (a: number | string, b: number | boolean) => {
+//   if (a === b) {
+//     console.log(a, b);
+//   }
+// };
+// const printReadings2 = (a: number[] | string) => {
+//   console.log(a.slice(0, 2));
+// };
+//
+// const checkReadings = (reading: { sysytem: number } | { user: number }): void => {
+//   if ('sysytem' in reading) {
+//     console.log(reading.sysytem);
+//   } else {
+//     console.log(reading.user);
+//   }
+// };
+//
+// function logValue(x: string | Date) {
+//   if (x instanceof Date) {
+//     console.log(x.getMinutes());
+//   } else {
+//     console.log(x.toUpperCase());
+//   }
+// }
+// //lesson #17 Tuples (Кортежи)
+//
+// type ObjType = {
+//   isBirthday: boolean, userName: string, age: number, messages: {
+//     error: string
+//   }
+// };
+//
+//
+//
+// let message: string | number = '';
+// let messages: string[] | number[] = ['m',"ss"];
+//
+// function printMess(msg:string|number) {
+//   console.log(msg);
+// }
+// //lesson #18 Union (Объединение)
+//
+// let message: string | number = '';
+// let messages: string[] | number[] = ['m',"ss"];
+//
+// function printMess(msg:string|number) {
+//   console.log(msg);
+// }
+//
+// // //lesson #17 Tuples (Кортежи)
+//
+//
+// type ObjType = {
+//   isBirthday: boolean, userName: string, age: number, messages: {
+//     error: string
+//   }
+// };
+//---
+//
+// const userData = {
+//   isBirthday: true,
+//   age: 40,
+//   userName: 'John',
+//   messages: {
+//     error: 'Error'
+//   }
+// };
+//
+//
+// // const userDataTuple:[boolean,number,string] = [true, 40, 'john'];
+// const userDataTuple: [boolean, number, ...string[]] = [true, 40, 'john', 'adas', 'asdas'];
+// // userDataTuple[3]=0
+// // console.log(userDataTuple);
+// // userDataTuple[0] = 'true';
+//
+// // userDataTuple.map((el)=> "throw new Error(el)")
+//---
+// const [isBirthday, age, userName] = userDataTuple;
+//
+// const createError = (msg: string): never => {
+//   throw new Error(msg);
+// };
+//
+// const logBrtMsg = ({isBirthday, userName, age, messages: {error}}: ObjType): string => {
+//   if (isBirthday) {
+//     return `Congrats ${userName}, age: ${age + 1}`;
+//   } else {
+//     return createError(error);
+//   }
+// };
+// // const logBrtMsg = (userData: ObjType): string => userData.isBirthday
+// //   ? `Congrats ${userData.userName}, age: ${userData.age + 1}`
+// //   : createError('Error');
+// console.log(logBrtMsg(userData));
+// //
+//
+// const departments: string[] = ['dev', 'design', 'marketing'];
+// const department: string = departments[0];
+// // departments.push(5)
+// const report = departments.filter(el => el !== 'dev').map(el => `${el} - done`);
+//
+// const nums: number[][] = [[1, 2], [3, 4], [5]];
+// const [first] = report;
+// console.log(first);
+//
+// // //lesson #14-15
+// type ObjType = {
+//   isBirthday: boolean, userName: string, age: number, messages: {
+//     error: string
+//   }
+// };
+// const userData = {
+//   isBirthday: true,
+//   age: 40,
+//   userName: 'John',
+//   messages: {
+//     error: 'Error'
+//   }
+// };
+//
+// const createError = (msg: string): never => {
+//   throw new Error(msg);
+// };
+// // let userData = {isBirthday: true, age: 40, userName: 'John'};
+// // let userObj: ObjType = JSON.parse(userData);
+//
+//
+// const logBrtMsg = ({isBirthday, userName, age, messages:{error}}: ObjType): string => {
+//   if (isBirthday) {
+//     return `Congrats ${userName}, age: ${age + 1}`;
+//   } else {
+//     return createError(error);
+//   }
+// };
+// // const logBrtMsg = (userData: ObjType): string => userData.isBirthday
+// //   ? `Congrats ${userData.userName}, age: ${userData.age + 1}`
+// //   : createError('Error');
+// console.log(logBrtMsg(userData));
+// //
+//
+// const departments: string[] = ['dev', 'design', 'marketing'];
+// const department: string = departments[0];
+// // departments.push(5)
+// const report = departments.filter(el => el !== 'dev').map(el => `${el} - done`);
+//
+// const nums: number[][] = [[1, 2], [3, 4], [5]];
+// const [first] = report;
+// console.log(first);
 // //lesson 12
 //
 // const test: null = null;
--- chapter_1/index.ts
+++ chapter_1/index.ts
@@ -3,66 +3,192 @@
 // //tsc --init
 // // tsc -help
 
-// //lesson #18 Union (Объединение)
-
-
-let message: string | number = '';
-let messages: string[] | number[] = ['m',"ss"];
-
-function printMess(msg:string|number) {
-  console.log(msg);
+// //lesson #21  .Объектные литералы и аннотации функций
+const serverConfig:{protocol: 'http' | 'https', port: 3000 | 3001}={
+  protocol:"https",
+  port:3001
 }
+function startServer(protocol: 'http' | 'https', port: 3000 | 3001): 'Serer started' {
+  console.log(`Serer started in ${protocol}: //server:${port}`);
+  return 'Serer started';
+}
+startServer(serverConfig.protocol, serverConfig.port)
+
+// //lesson #21  Псевдонимы типов (Type aliases)
+//
+// type timingFuncType = 'ease' | 'ease-out' | 'ease-in'
+// type idType = string | number
+// const createAnimation = (id: string | number, animationName: string, timingFunc: timingFuncType,
+//                          duration: number, iterCount: 'infinite' | number): void => {
+//   const elem = document.querySelector(`#${id}`) as HTMLElement;
+//   if (elem && elem.style) {
+//     console.log(`${animationName} ${timingFunc} ${duration} ${iterCount}`);
+//     // elem.style.animation = `${animationName} ${timingFunc} ${duration} ${iterCount}`;
+//     // console.log(elem.style.animation);
+//   }
+// };
+
+
+// //lesson #20 Примитивные литеральные типы (Literal types)
+
+// let msg: 'hello' | 'Hello' = 'hello';
+// msg = 'Hello';
+//
+// const port3000: number = 3000;
+// const port3001: number = 3001;
+//
+// function startServer(protocol: 'http' | 'https', port: 3000 | 3001): 'Serer started' {
+//   if (port === port3000 || port === port3001) {
+//     console.log(`Serer started in ${protocol}: //server:${port}`);
+//   } else {
+//     console.log('Error!!!');
+//   }
+//   return 'Serer started';
+// }
+//
+// const createAnimation = (id: string | number, animationName: string, timingFunc: 'ease' | 'ease-out' | 'ease-in' = 'ease',
+//                          duration: number, iterCount: 'infinite' | number): void => {
+//   const elem = document.querySelector(`#${id}`) as HTMLElement;
+//   if (elem && elem.style) {
+//     console.log(`${animationName} ${timingFunc} ${duration} ${iterCount}`);
+//     // elem.style.animation = `${animationName} ${timingFunc} ${duration} ${iterCount}`;
+//     // console.log(elem.style.animation);
+//   }
+// };
+
+// //lesson #19 Сужение типов (Narrowing)
+//
+//
+// let message: string | number = '';
+// let messages: string[] | number[] = ['m', 'ss'];
+// //
+// // function printMess(msg: string | number|boolean) {
+// //   if (typeof msg !== 'number'||typeof msg==="number") {
+// //     console.log(msg.toString());
+// //   } else {
+// //     console.log(msg);
+// //   }
+// //   console.log(msg)
+// // }
+// function printMess(msg: string[] | number | boolean) {
+//   if (Array.isArray(msg)) {
+//     msg.forEach(el => console.log(el));
+//   } else if (typeof msg === 'number') {
+//     console.log(msg.toFixed());
+//   } else {
+//     console.log(msg);
+//   }
+// }
+//
+// const printReadings = (a: number | string, b: number | boolean) => {
+//   if (a === b) {
+//     console.log(a, b);
+//   }
+// };
+// const printReadings2 = (a: number[] | string) => {
+//   console.log(a.slice(0, 2));
+// };
+//
+// const checkReadings = (reading: { sysytem: number } | { user: number }): void => {
+//   if ('sysytem' in reading) {
+//     console.log(reading.sysytem);
+//   } else {
+//     console.log(reading.user);
+//   }
+// };
+//
+// function logValue(x: string | Date) {
+//   if (x instanceof Date) {
+//     console.log(x.getMinutes());
+//   } else {
+//     console.log(x.toUpperCase());
+//   }
+// }
 
 // //lesson #17 Tuples (Кортежи)
 
-
-type ObjType = {
-  isBirthday: boolean, userName: string, age: number, messages: {
-    error: string
-  }
-};
-const userData = {
-  isBirthday: true,
-  age: 40,
-  userName: 'John',
-  messages: {
-    error: 'Error'
-  }
-};
-// const userDataTuple:[boolean,number,string] = [true, 40, 'john'];
-const userDataTuple: [boolean, number, ...string[]] = [true, 40, 'john', 'adas', 'asdas'];
-// userDataTuple[3]=0
-// console.log(userDataTuple);
-// userDataTuple[0] = 'true';
-
-// userDataTuple.map((el)=> "throw new Error(el)")
-const [isBirthday, age, userName] = userDataTuple;
-
-const createError = (msg: string): never => {
-  throw new Error(msg);
-};
-
-const logBrtMsg = ({isBirthday, userName, age, messages: {error}}: ObjType): string => {
-  if (isBirthday) {
-    return `Congrats ${userName}, age: ${age + 1}`;
-  } else {
-    return createError(error);
-  }
-};
-// const logBrtMsg = (userData: ObjType): string => userData.isBirthday
-//   ? `Congrats ${userData.userName}, age: ${userData.age + 1}`
-//   : createError('Error');
-console.log(logBrtMsg(userData));
 //
+// type ObjType = {
+//   isBirthday: boolean, userName: string, age: number, messages: {
+//     error: string
+//   }
+// };
+//
+//
+//
+// let message: string | number = '';
+// let messages: string[] | number[] = ['m',"ss"];
+//
+// function printMess(msg:string|number) {
+//   console.log(msg);
+// }
+// //lesson #18 Union (Объединение)
 
-const departments: string[] = ['dev', 'design', 'marketing'];
-const department: string = departments[0];
-// departments.push(5)
-const report = departments.filter(el => el !== 'dev').map(el => `${el} - done`);
+//
+// let message: string | number = '';
+// let messages: string[] | number[] = ['m',"ss"];
+//
+// function printMess(msg:string|number) {
+//   console.log(msg);
+// }
+//
+// // //lesson #17 Tuples (Кортежи)
+//
+//
+// type ObjType = {
+//   isBirthday: boolean, userName: string, age: number, messages: {
+//     error: string
+//   }
+// };
+//---
+//
+// const userData = {
+//   isBirthday: true,
+//   age: 40,
+//   userName: 'John',
+//   messages: {
+//     error: 'Error'
+//   }
+// };
+//
+//
+// // const userDataTuple:[boolean,number,string] = [true, 40, 'john'];
+// const userDataTuple: [boolean, number, ...string[]] = [true, 40, 'john', 'adas', 'asdas'];
+// // userDataTuple[3]=0
+// // console.log(userDataTuple);
+// // userDataTuple[0] = 'true';
+//
+// // userDataTuple.map((el)=> "throw new Error(el)")
 
-const nums: number[][] = [[1, 2], [3, 4], [5]];
-const [first] = report;
-console.log(first);
+
+//---
+// const [isBirthday, age, userName] = userDataTuple;
+//
+// const createError = (msg: string): never => {
+//   throw new Error(msg);
+// };
+//
+// const logBrtMsg = ({isBirthday, userName, age, messages: {error}}: ObjType): string => {
+//   if (isBirthday) {
+//     return `Congrats ${userName}, age: ${age + 1}`;
+//   } else {
+//     return createError(error);
+//   }
+// };
+// // const logBrtMsg = (userData: ObjType): string => userData.isBirthday
+// //   ? `Congrats ${userData.userName}, age: ${userData.age + 1}`
+// //   : createError('Error');
+// console.log(logBrtMsg(userData));
+// //
+//
+// const departments: string[] = ['dev', 'design', 'marketing'];
+// const department: string = departments[0];
+// // departments.push(5)
+// const report = departments.filter(el => el !== 'dev').map(el => `${el} - done`);
+//
+// const nums: number[][] = [[1, 2], [3, 4], [5]];
+// const [first] = report;
+// console.log(first);
 
 
 //
